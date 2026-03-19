2af7ed0ac20889c09711e1f3c4786c0ce7cb7f2e
Aditya Singh <1211821022@ngpp.ac.in>
2026-03-20 02:26:29 +0530

    implement user registration and login functionality with appropriate routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createServer } from 'node:http';
-import { connectToSocket } from './controllers/socketManager.js'
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
+import { connectToSocket } from './controllers/socketManager.js'
+import userRoutes from './routes/users.routes.js'
 
 const app = express();
 const server = createServer(app);
@@ -14,6 +15,8 @@ app.set("port", (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/users", userRoutes);
 
 const start = async () => {
 
---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,105 @@
+import httpStatus from 'http-status';
+import bcrypt from 'bcrypt';
+import crypto from 'crypto'
+import { User } from '../models/user.model.js';
+
+const register = async (req, res) => {
+    const { name, username, password } = req.body;
+
+    try {
+        if (!name || !username || !password) {
+            console.log("Data Missing");
+            return res
+                .status(httpStatus.NOT_FOUND)
+                .json({
+                    "message": "Data is Missing!",
+                    "success": false
+                });
+        }
+
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res
+                .status(httpStatus.FOUND)
+                .json({
+                    "message": "User already exists",
+                    "success": false
+                });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User(
+            {
+                name: name,
+                username: username,
+                password: hashedPassword
+            }
+        );
+
+        await newUser.save();
+
+        res
+            .status(httpStatus.CREATED)
+            .json({
+                "message": "User registered successfully",
+                "success": true
+            })
+
+    } catch (error) {
+        res
+            .status(httpStatus.INTERNAL_SERVER_ERROR)
+            .json({
+                "message": `Something went wrong! ${error}`
+            })
+    }
+}
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        if (!username || !password) {
+            console.log("Data Missing");
+            return res
+                .status(httpStatus.BAD_REQUEST)
+                .json({
+                    "message": "Data is Missing!",
+                    "success": false
+                });
+        }
+
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res
+                .status(httpStatus.NOT_FOUND)
+                .json({
+                    "message": "User don't exists",
+                    "success": false
+                });
+        }
+
+        if (bcrypt.compare(password, user.password)) {
+            let token = crypto.randomBytes(20).toString('hex');
+
+            user.token = token;
+            await user.save();
+            return res
+                .status(httpStatus.OK)
+                .json({
+                    "message": "User logged in successfuly",
+                    "token": token,
+                    "success": true
+                })
+        }
+    } catch (error) {
+        res
+            .status(httpStatus.INTERNAL_SERVER_ERROR)
+            .json({
+                "message": `Something went wrong! ${error}`
+            })
+    }
+}
+
+export { login, register }
